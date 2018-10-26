d04f93d9f8d94a770dfd70b825832b3780f51d06
jhoss64 <42900330+jhoss64@users.noreply.github.com>
2018-10-25 21:19:44 -0500

    Update src/main/java/com/grupo01/application/EditorWindow.java

--- src/main/java/com/grupo01/application/EditorWindow.java
+++ src/main/java/com/grupo01/application/EditorWindow.java
@@ -6,17 +6,23 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JSeparator;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JScrollBar;
 
 public class EditorWindow {
 
-	private JFrame frame;
+	private JFrame frmEditor;
 
 	/**
 	 * Launch the application.
@@ -26,7 +32,7 @@ public class EditorWindow {
 			public void run() {
 				try {
 					EditorWindow window = new EditorWindow();
-					window.frame.setVisible(true);
+					window.frmEditor.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,26 +51,102 @@ public class EditorWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 608, 550);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmEditor = new JFrame();
+		frmEditor.setFont(new Font("Trebuchet MS", Font.PLAIN, 12));
+		frmEditor.setTitle("Editor 3000");
+		frmEditor.setBounds(100, 100, 744, 582);
+		frmEditor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
+		frmEditor.setJMenuBar(menuBar);
 		
 		JMenu mnNewMenu = new JMenu("Archivo");
 		menuBar.add(mnNewMenu);
 		
 		JMenuItem mntmNuevo = new JMenuItem("Nuevo");
+		mntmNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				EditorWindow window = new EditorWindow();
+				window.frmEditor.setVisible(true);
+			}
+		});
 		mnNewMenu.add(mntmNuevo);
 		
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		mntmAbrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mensaje();
+			}
+		});
+		mnNewMenu.add(mntmAbrir);
+		
+		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+		mntmGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mensaje();
+			}
+		});
+		mnNewMenu.add(mntmGuardar);
+		
+		JSeparator separator = new JSeparator();
+		mnNewMenu.add(separator);
+		
+		JMenuItem mntmEnConstruccin_1 = new JMenuItem("En construcción...");
+		mntmEnConstruccin_1.setForeground(Color.BLUE);
+		mnNewMenu.add(mntmEnConstruccin_1);
+		
+		JMenu mnFormato = new JMenu("Formato");
+		menuBar.add(mnFormato);
+		
+		JMenuItem mntmTamaoDeLetra = new JMenuItem("Tamaño de letra");
+		mntmTamaoDeLetra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mensaje();
+			}
+		});
+		mnFormato.add(mntmTamaoDeLetra);
+		
+		JMenuItem mntmFormatoDeLetra = new JMenuItem("Formato de letra");
+		mntmFormatoDeLetra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mensaje();
+			}
+		});
+		mnFormato.add(mntmFormatoDeLetra);
+		
+		JMenu mnCerrar = new JMenu("Cerrar");
+		menuBar.add(mnCerrar);
+		
+		JMenuItem mntmEnConstruccin = new JMenuItem("En construcción...");
+		mntmEnConstruccin.setForeground(Color.BLUE);
+		mnCerrar.add(mntmEnConstruccin);
+		
 		JToolBar toolBar = new JToolBar();
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		frmEditor.getContentPane().add(toolBar, BorderLayout.NORTH);
 		
 		JButton btnNewButton = new JButton("Guardar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mensaje();
+			}
+		});
 		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
 		btnNewButton.setIcon(new ImageIcon("D:\\Personal\\MAESTRIA\\ADSW\\GPS_Editor_G1\\src\\main\\resources\\icons\\save-icon.png"));
 		toolBar.add(btnNewButton);
+		
+		JTextArea textArea = new JTextArea();
+		frmEditor.getContentPane().add(textArea, BorderLayout.CENTER);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		frmEditor.getContentPane().add(scrollBar, BorderLayout.EAST);
+		
+		JScrollBar scrollBar_1 = new JScrollBar();
+		scrollBar_1.setOrientation(JScrollBar.HORIZONTAL);
+		frmEditor.getContentPane().add(scrollBar_1, BorderLayout.SOUTH);
+	
+		
 	}
-
+	private void mensaje() {
+		JOptionPane.showMessageDialog(null, "Opción disponible en el siguiente entregable","Trabajamos para ti...!", 1);
+	}
 }
